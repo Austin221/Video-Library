6f2483c5b1b12cefbbbf49f568ea1e662feb5ada
Austin221 <Austin221@users.noreply.github.com>
2017-01-28 15:39:02 -0800

    updated mega links options.
    For links with no code enter "none" without "" and it will not enter the key.

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
   using namespace std;
-  string test, show, search;
+  string test, show, search, movie, link, key;
   int method;
 
   for(;;) {
@@ -18,14 +18,17 @@ int main()
   	cout << "               2. Search for a show.                          \n";
   	cout << "               3. Add a show to the list.                     \n";
   	cout << "               4. Add link and encryption key (if any).       \n";
-  	cout << "               5. EXIT THE PROGRAM                            \n";
+  	cout << "               5. Add Movie                                   \n";
+  	cout << "               6. EXIT THE PROGRAM                            \n";
   	cout << "                                                              \n";
     cin >> method;
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     switch(method) {
       case 1:
+          cout << " \n";
         cout << string(istreambuf_iterator<char>(myfile_in), {});
+        cout << "\n";
       break;
     	case 2:
         cout << "What would you like to find? \n";
@@ -35,14 +38,34 @@ int main()
       case 3:
       	cout << "Enter the name of the show you would like to add. \n";
       	cin >> show;
+      	cout << " \n";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         myfile_out << show << endl;
       break;
       case 4:
-        // FIXME: Make the links stored in another file
+        cout << "Enter the name of the show you would like to add. \n";
+      	cin >> link;
+      	cout << " \n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        myfile_out << "Mega-Link: " << link << endl;
+        cout << "Enter in your Mega-Link key. \n";
+        cin >> key;
+          if (key == "none") {
+        break;
+        }
+        myfile_out << "Mega-Key: " << key << endl;
       break;
       case 5:
+          cout << "Enter the name of the movie you would like to add. \n";
+      	cin >> movie;
+      	cout << " \n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        myfile_out << "---MOVIE--- " << movie << endl;
+        break;
+      case 6:
         cout << "Exiting the program!";
         return 0;
         break;
